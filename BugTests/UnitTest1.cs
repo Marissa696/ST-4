912c9f3ad0c1c7d2c799673fbfcecd358a360e16
Marissa696 <mrnsrtv@gmail.com>
2024-05-30 15:22:01 +0300

    fix tests

--- BugTests/UnitTest1.cs
+++ BugTests/UnitTest1.cs
@@ -1,10 +1,10 @@
 namespace BugTests;
 
 [TestClass]
-public class UnitTest
+public class UnitTest1
 {
     [TestMethod]
-    public void Test_Assign_To_Assigned()
+    public void Test_AssignToAssigned()
     {
         var bug = new Bug(Bug.State.Open);
         bug.Assign();
@@ -13,7 +13,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Assign_To_Closed()
+    public void Test_AssignToClosed()
     {
         var bug = new Bug(Bug.State.Assigned);
         bug.Close();
@@ -22,7 +22,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Close_While_Open()
+    public void Test_CloseWhileOpen()
     {
         var bug = new Bug(Bug.State.Open);
         bug.Close();
@@ -31,7 +31,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Defer_While_Assigned()
+    public void Test_DeferWhileAssigned()
     {
         var bug = new Bug(Bug.State.Assigned);
         bug.Defer();
@@ -40,7 +40,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Defer_While_Closed()
+    public void Test_DeferWhileClosed()
     {
         var bug = new Bug(Bug.State.Closed);
         bug.Defer();
@@ -49,7 +49,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Assign_While_Closed()
+    public void Test_AssignWhileClosed()
     {
         var bug = new Bug(Bug.State.Closed);
         bug.Assign();
@@ -58,7 +58,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Close_While_Assigned()
+    public void Test_CloseWhileAssigned()
     {
         var bug = new Bug(Bug.State.Assigned);
         bug.Close();
@@ -67,7 +67,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Assign_While_Deferred()
+    public void Test_AssignWhileDeferred()
     {
         var bug = new Bug(Bug.State.Defered);
         bug.Assign();
@@ -76,7 +76,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Defer_While_Deferred()
+    public void Test_DeferWhileDeferred()
     {
         var bug = new Bug(Bug.State.Defered);
         bug.Defer();
@@ -85,7 +85,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Close_While_Deferred()
+    public void Test_CloseWhileDeferred()
     {
         var bug = new Bug(Bug.State.Defered);
         bug.Close();
@@ -94,7 +94,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Assign_While_Assigned()
+    public void Test_AssignWhileAssigned2()
     {
         var bug = new Bug(Bug.State.Assigned);
         bug.Assign();
@@ -103,7 +103,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Assign_While_Open()
+    public void Test_AssignWhileOpen()
     {
         var bug = new Bug(Bug.State.Open);
         bug.Assign();
@@ -112,7 +112,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Defer_While_Closed()
+    public void Test_DeferWhileClosed2()
     {
         var bug = new Bug(Bug.State.Closed);
         bug.Defer();
@@ -121,7 +121,7 @@ public class UnitTest
     }
 
     [TestMethod]
-    public void Test_Close_While_Open()
+    public void Test_CloseWhileOpen2()
     {
         var bug = new Bug(Bug.State.Open);
         bug.Close();
