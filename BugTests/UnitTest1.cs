761fc696e29d5dd3dcce3fa6814977f20fe24f0c
Marissa696 <mrnsrtv@gmail.com>
2024-05-30 15:15:39 +0300

    fix tests

--- BugTests/UnitTest1.cs
+++ BugTests/UnitTest1.cs
@@ -4,75 +4,128 @@ namespace BugTests;
 public class UnitTest1
 {
     [TestMethod]
-    public void Test_Assign_To_Defer()
-    {
-        var bug = new Bug(Bug.State.Assigned);
-        bug.Defer();
-        Bug.State state = bug.getState();
-        Assert.AreEqual(Bug.State.Defered, state);
-    }
-     [TestMethod]
-    public void Test_Open_To_Assign()
+    public void Test_Assign_To_Assigned()
     {
         var bug = new Bug(Bug.State.Open);
         bug.Assign();
         Bug.State state = bug.getState();
         Assert.AreEqual(Bug.State.Assigned, state);
     }
+
     [TestMethod]
-    public void Test_Assign_To_Close()
+    public void Test_Assign_To_Closed()
     {
         var bug = new Bug(Bug.State.Assigned);
         bug.Close();
         Bug.State state = bug.getState();
         Assert.AreEqual(Bug.State.Closed, state);
     }
+
     [TestMethod]
-    public void Test_Assign_To_Assign()
+    public void Test_Close_While_Open()
+    {
+        var bug = new Bug(Bug.State.Open);
+        bug.Close();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Open, state);
+    }
+
+    [TestMethod]
+    public void Test_Defer_While_Assigned()
+    {
+        var bug = new Bug(Bug.State.Assigned);
+        bug.Defer();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Defered, state);
+    }
+
+    [TestMethod]
+    public void Test_Defer_While_Closed()
+    {
+        var bug = new Bug(Bug.State.Closed);
+        bug.Defer();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Closed, state);
+    }
+
+    [TestMethod]
+    public void Test_Assign_While_Closed()
+    {
+        var bug = new Bug(Bug.State.Closed);
+        bug.Assign();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Closed, state);
+    }
+
+    [TestMethod]
+    public void Test_Close_While_Assigned()
+    {
+        var bug = new Bug(Bug.State.Assigned);
+        bug.Close();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Closed, state);
+    }
+
+    [TestMethod]
+    public void Test_Assign_While_Deferred()
+    {
+        var bug = new Bug(Bug.State.Defered);
+        bug.Assign();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Assigned, state);
+    }
+
+    [TestMethod]
+    public void Test_Defer_While_Deferred()
+    {
+        var bug = new Bug(Bug.State.Defered);
+        bug.Defer();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Defered, state);
+    }
+
+    [TestMethod]
+    public void Test_Close_While_Deferred()
+    {
+        var bug = new Bug(Bug.State.Defered);
+        bug.Close();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Closed, state);
+    }
+
+    [TestMethod]
+    public void Test_Assign_While_Assigned()
     {
         var bug = new Bug(Bug.State.Assigned);
         bug.Assign();
         Bug.State state = bug.getState();
         Assert.AreEqual(Bug.State.Assigned, state);
     }
+
     [TestMethod]
-    public void Test_Close_To_Assign()
+    public void Test_Assign_While_Open()
     {
-        var bug = new Bug(Bug.State.Closed);
+        var bug = new Bug(Bug.State.Open);
         bug.Assign();
         Bug.State state = bug.getState();
         Assert.AreEqual(Bug.State.Assigned, state);
     }
+
     [TestMethod]
-    public void Test_Defer_To_Assign()
-    {
-        var bug = new Bug(Bug.State.Defered);
-        bug.Assign();
-        Bug.State state = bug.getState();
-        Assert.AreEqual(Bug.State.Assigned, state);
-    }
-    [TestMethod]
-    public void Test_Defer_To_Close_Exception()
-    {
-        var bug = new Bug(Bug.State.Defered);
-        Assert.ThrowsException<InvalidOperationException>(() => bug.Close());
-    }
-    [TestMethod]
-    public void Test_Close_To_Defer_Exception()
+    public void Test_Defer_While_Closed()
     {
         var bug = new Bug(Bug.State.Closed);
-        Assert.ThrowsException<InvalidOperationException>(() => bug.Defer());
-    }  
+        bug.Defer();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Closed, state);
+    }
+
     [TestMethod]
-    public void Test_Open_To_Defer_Exception()
+    public void Test_Close_While_Open()
     {
         var bug = new Bug(Bug.State.Open);
-        Assert.ThrowsException<InvalidOperationException>(() => bug.Defer());
-    }      
-    [TestMethod]
-    public void Test_Open_To_Close_Exception()
-    {
-        var bug = new Bug(Bug.State.Open);
-        Assert.ThrowsException<InvalidOperationException>(() => bug.Close());
+        bug.Close();
+        Bug.State state = bug.getState();
+        Assert.AreEqual(Bug.State.Open, state);
     }
 }
